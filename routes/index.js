15511bb1ec1610138caf90d7ff137bf64260b734
Ejinks <ejinks2@stevens.edu>
2025-04-10 14:29:29 -0400

    Beginning home page development.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 import adminRoute from "./adminRoutes.js";
+import { Router } from "express";
+const router = Router();
+router.route('').get(async (req, res) => {
+    res.render('home', { title: 'Homepage' })
+});
 
 const setRoutes = (app) => {
-    app.use('/', adminRoute);
+    app.use('/', router);
+
+    app.use('/admin', adminRoute);
 
     app.use(/(.*)/, (req, res) => {
         res.status(404).json({ error: "Page not found. "});
