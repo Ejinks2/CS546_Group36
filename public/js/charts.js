25572c0fedd1cc3ae37adc1c4b9d612209c1adaa
mbobkov26 <mbobkov@stevens.edu>
2025-05-08 18:08:59 -0400

    Month based charts
    Added month based charts that organize by crime type

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,4 +1,6 @@
-function updateChartb (crimeData, chartElem, chart) {
+const monthRef = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "August", "09": "September", "10": "October", "11": "November", "12": "December"};
+
+function updateChartB (crimeData, chartElem, chart, borough) {
   const boroughCrimes = {};
   crimeData.forEach(crime => {
     boroughCrimes[crime.offense] = (boroughCrimes[crime.offense] || 0) + 1;
@@ -6,12 +8,15 @@ function updateChartb (crimeData, chartElem, chart) {
 
   if (chart) chart.destroy();
 
+  let label = "Number of Crimes";
+  if (borough !== "ALL") label = "Number of Crimes in " + borough;
+
   return new Chart(chartElem, {
     type: 'bar',
     data: {
       labels: Object.keys(boroughCrimes),
       datasets: [{
-        label: 'Number of Crimes',
+        label: label,
         data: Object.values(boroughCrimes),
       }]
     },
@@ -35,7 +40,7 @@ function updateChartb (crimeData, chartElem, chart) {
   });
 }
 
-function updateChartc (crimeData, chartElem, chart) {
+function updateChartC (crimeData, chartElem, chart, offense) {
   const boroughCrimes = {};
   crimeData.forEach(crime => {
     boroughCrimes[crime.borough] = (boroughCrimes[crime.borough] || 0) + 1;
@@ -43,12 +48,15 @@ function updateChartc (crimeData, chartElem, chart) {
 
   if (chart) chart.destroy();
 
+  let label = "Number of Crimes per borough";
+  if (offense !== "ALL") label = "Number of " + offense + " crimes per borough";
+
   return new Chart(chartElem, {
     type: 'bar',
     data: {
       labels: Object.keys(boroughCrimes),
       datasets: [{
-        label: 'Number of Crimes',
+        label: label,
         data: Object.values(boroughCrimes),
       }]
     },
@@ -71,3 +79,44 @@ function updateChartc (crimeData, chartElem, chart) {
     }
   });
 }
+
+function updateChartT (crimeData, chartElem, chart, month) {
+  const boroughCrimes = {};
+  crimeData.forEach(crime => {
+    boroughCrimes[crime.offense] = (boroughCrimes[crime.offense] || 0) + 1;
+  });
+
+  if (chart) chart.destroy();
+
+  let label = "Number of Crimes in " + monthRef[month];
+  if (month === "ALL") label = "Number of Crimes";
+
+  return new Chart(chartElem, {
+    type: 'bar',
+    data: {
+      labels: Object.keys(boroughCrimes),
+      datasets: [{
+        label: label,
+        data: Object.values(boroughCrimes),
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          title: {
+            display: true,
+            text: 'Number of Offenses'
+          }
+        },
+        x: {
+          title: {
+            display: true,
+            text: 'Crime'
+          }
+        }
+      }
+    }
+  });
+}
+
