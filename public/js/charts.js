588127d5a99e0c1b9bf04bd9b79915f444f4c1f8
mbobkov26 <mbobkov@stevens.edu>
2025-05-07 20:42:31 -0400

    Chart axes change
    Switched chart axes such that selecting a borough displays filters crime types and selecting a crime type filters by borough

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,6 +1,43 @@
 function updateChartb (crimeData, chartElem, chart) {
   const boroughCrimes = {};
   crimeData.forEach(crime => {
+    boroughCrimes[crime.offense] = (boroughCrimes[crime.offense] || 0) + 1;
+  });
+
+  if (chart) chart.destroy();
+
+  return new Chart(chartElem, {
+    type: 'bar',
+    data: {
+      labels: Object.keys(boroughCrimes),
+      datasets: [{
+        label: 'Number of Crimes',
+        data: Object.values(boroughCrimes),
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          title: {
+            display: true,
+            text: 'Number of Offenses'
+          }
+        },
+        x: {
+          title: {
+            display: true,
+            text: 'Crime'
+          }
+        }
+      }
+    }
+  });
+}
+
+function updateChartc (crimeData, chartElem, chart) {
+  const boroughCrimes = {};
+  crimeData.forEach(crime => {
     boroughCrimes[crime.borough] = (boroughCrimes[crime.borough] || 0) + 1;
   });
 
@@ -11,7 +48,7 @@ function updateChartb (crimeData, chartElem, chart) {
     data: {
       labels: Object.keys(boroughCrimes),
       datasets: [{
-        label: 'Number of Crimes per Borough',
+        label: 'Number of Crimes',
         data: Object.values(boroughCrimes),
       }]
     },
@@ -21,7 +58,7 @@ function updateChartb (crimeData, chartElem, chart) {
           beginAtZero: true,
           title: {
             display: true,
-            text: 'Crime Count'
+            text: 'Number of Offenses'
           }
         },
         x: {
