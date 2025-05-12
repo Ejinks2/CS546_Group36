ba94ae45160373f2438e1ec95246392c927568ed
Mick <mickbarbi@gmail.com>
2025-05-11 23:50:22 -0400

    added the admin pending reports page, and added functionality for accepting and rejecting crimes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,44 +1,55 @@
 import { Router } from "express";
 import { getAllReports, updateReportStatus } from "../data/userReports.js";
+import { ObjectId } from "mongodb";
 
 
 const router = Router();
 
 router.route('').get(async (req, res) => {
-    res.render('admin', { title: "Admin Page" });
+  res.render('admin', { title: "Admin Page" });
 })
 
 // View pending reports
 router.route('/reports').get(async (req, res) => {
-    try {
-      const reports = await getAllReports({ status: 'pending' });
-      res.render('admin', { title: "Pending User Reports", reports });
-    } catch (e) {
-      res.status(500).render('admin', { title: "Pending User Reports", error: e });
-    }
+  try {
+    const reports = await getAllReports({ status: 'pending' });
+    const finalReports = reports.map(obj => {
+      const {
+        _id,
+        ...keep
+      } = obj;
+      return {
+        ...keep,
+        _id: _id.toString()
+      }
+    })
+    res.render('admin', { title: "Pending User Reports", reports: finalReports });
+  } catch (e) {
+    res.status(500).render('admin', { title: "Pending User Reports", error: e });
+  }
 });
 
 // Approve a report
 router.route('/approve/:id').post(async (req, res) => {
-    try {
-        const id = req.params.id;
-        await updateReportStatus(id, 'approved');
-        res.redirect('/admin/reports');
-      } catch (e) {
-        res.status(400).render('admin', { title: "Error Approving Report", error: e });
-      }
+  try {
+    const id = req.params.id;
+    await updateReportStatus(id, 'approved');
+    res.redirect('/admin/reports');
+  } catch (e) {
+    res.status(400).render('admin', { title: "Error Approving Report", error: e });
+  }
 });
 
 // Reject a report
 router.route('/reject/:id').post(async (req, res) => {
-    try {
-      const id = req.params.id;
-      await updateReportStatus(id, 'rejected');
-      res.redirect('/admin/reports');
-    } catch (e) {
-      res.status(400).render('admin', { title: "Error Rejecting Report", error: e });
-    }
-  });
+  try {
+    const id = req.params.id;
+    await updateReportStatus(id, 'rejected');
+    res.redirect('/admin/reports');
+  } catch (e) {
+    res.status(400).render('admin', { title: "Error Rejecting Report", error: e });
+  }
+});
 
 router.route('/comments').get(async (req, res) => {
 
